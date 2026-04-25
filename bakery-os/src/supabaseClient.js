939db5d06b79eb8b86be799d629fd5279e53e556
thread101 <localthread101@gmail.com>
2026-04-25 12:25:39 +0300

    token fix

--- bakery-os/src/supabaseClient.js
+++ bakery-os/src/supabaseClient.js
@@ -6,7 +6,7 @@ const supabaseKey = 'sb_publishable_N2xzFMnQRUZN14_qXVo_kw_N0VcWU-I';
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
-    storage: window.sessionStorage,
+    storage: window.localStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true // Helps with recovery
